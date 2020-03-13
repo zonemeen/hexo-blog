52d05853d80fc28bc1a338f62eafe544855543f2
miqilin21 <994718917@qq.com>
2020-03-14 00:53:28 +0800

    Site updated: 2020-03-14 00:53:19

--- js/main.js
+++ js/main.js
@@ -1,28 +1,44 @@
 function scrollToElement(target, offset) {
   var scroll_offset = $(target).offset();
-  $("body,html").animate({
+  $('body,html').animate({
     scrollTop: scroll_offset.top + (offset || 0),
-    easing: 'swing'
-  })
+    easing: 'swing',
+  });
 }
 
 function scrollToBoard() {
-  scrollToElement('#board', -$("#navbar").height());
+  scrollToElement('#board', -$('#navbar').height());
+}
+
+// 防抖动函数
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function () {
+    var context = this, args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
 }
 
 $(document).ready(function () {
   // 顶部菜单的动效
-  var navbar = $("#navbar");
+  var navbar = $('#navbar');
   if (navbar.offset().top > 0) {
-    navbar.addClass("navbar-custom");
-    navbar.removeClass("navbar-dark");
+    navbar.addClass('navbar-custom');
+    navbar.removeClass('navbar-dark');
   }
   $(window).scroll(function () {
     if (navbar.offset().top > 0) {
-      navbar.addClass("navbar-custom");
-      navbar.removeClass("navbar-dark");
+      navbar.addClass('navbar-custom');
+      navbar.removeClass('navbar-dark');
     } else {
-      navbar.addClass("navbar-dark");
+      navbar.addClass('navbar-dark');
     }
   });
   $('#navbar-toggler-btn').on('click', function () {
@@ -30,11 +46,39 @@ $(document).ready(function () {
     $('#navbar').toggleClass('navbar-col-show');
   });
 
+  // 头图视差动画
+  var target = $('#background[parallax="true"]');
+  var parallax = function () {
+    var oVal = $(window).scrollTop() / 5;
+    var offset = parseInt($('#board').css('margin-top'));
+    var max = 96 + offset;
+    if (oVal > max) {
+      oVal = max;
+    }
+    target.css({
+      transform: 'translate3d(0,' + oVal + 'px,0)',
+      '-webkit-transform': 'translate3d(0,' + oVal + 'px,0)',
+      '-ms-transform': 'translate3d(0,' + oVal + 'px,0)',
+      '-o-transform': 'translate3d(0,' + oVal + 'px,0)',
+    });
+
+    var toc = $('#toc');
+    if (toc) {
+      $('#toc-ctn').css({
+        'padding-top': `${ oVal }px`,
+      });
+    }
+  };
+  if (target.length > 0) {
+    parallax();
+    $(window).scroll(parallax);
+  }
+
   // 向下滚动箭头的点击
-  $(".scroll-down-bar").on("click", scrollToBoard);
+  $('.scroll-down-bar').on('click', scrollToBoard);
 
   // 向顶部滚动箭头
-  var topArrow = $("#scroll-top-button");
+  var topArrow = $('#scroll-top-button');
   var posDisplay = false;
   var scrollDisplay = false;
   // 位置
@@ -44,31 +88,26 @@ $(document).ready(function () {
     var right = bodyWidth - boardRight;
     posDisplay = right >= 50;
     topArrow.css({
-      "bottom": posDisplay && scrollDisplay ? "20px" : "-60px",
-      "right": right - 64 + "px"
+      'bottom': posDisplay && scrollDisplay ? '20px' : '-60px',
+      'right': right - 64 + 'px',
     });
   };
   setTopArrowPos();
   $(window).resize(setTopArrowPos);
   // 显示
-  var headerHeight = $("#board").offset().top;
-  $(window).scroll(function () {
+  var headerHeight = $('#board').offset().top;
+  $(window).scroll(debounce(function () {
     var scrollHeight = document.body.scrollTop + document.documentElement.scrollTop;
     scrollDisplay = scrollHeight >= headerHeight;
     topArrow.css({
-      "bottom": posDisplay && scrollDisplay ? "20px" : "-60px"
+      'bottom': posDisplay && scrollDisplay ? '20px' : '-60px',
+    });
+  }, 20));
+  // 点击
+  topArrow.on('click', function () {
+    $('body,html').animate({
+      scrollTop: 0,
+      easing: 'swing',
     });
   });
-  // 点击
-  topArrow.on("click", function () {
-    $("body,html").animate({
-      scrollTop: 0,
-      easing: 'swing'
-    })
-  });
-
-  // 因兼容问题，在 iOS 和 Safari 环境下不使用固定 Banner
-  if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent) || (/Safari/i.test(navigator.userAgent) && !/Chrome/i.test(navigator.userAgent))) {
-    $("#background").css("background-attachment", "scroll");
-  }
 });
